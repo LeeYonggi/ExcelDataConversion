d84a5e2e1d40fbb9187cc86289498e32ce2fcce7
LeeYonggi <rl4459@naver.com>
2019-12-13 15:56:36 +0900

    버전 1.0.0

---
+++ ExcelDataConversion/ExcelDataConversion/ExcelDataParser/ExcelDataParser.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelDataConversion
+{
+
+    public static class ExcelDataParser
+    {
+        public static DataTable ExcelFileRead(string filePath)
+        {
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook workbook = app.Workbooks.Open(filePath);
+            Excel.Worksheet sheet = workbook.Worksheets.Item[1] as Excel.Worksheet;
+
+            Excel.Range range = sheet.UsedRange;
+
+            object[,] data = range.Value;
+
+            DataTable dataTable = new DataTable();
+            for(int i = 1; i <= range.Columns.Count; i++)
+            {
+                dataTable.Columns.Add(i.ToString(), typeof(string));
+            }
+
+            for (int r = 1; r <= range.Rows.Count; r++)
+            {
+                DataRow dr = dataTable.Rows.Add();
+
+                for (int c = 1; c <= range.Columns.Count; c++)
+                {
+                    dr[c - 1] = data[r, c];
+                }
+            }
+
+            workbook.Close(true);
+            app.Quit();
+            DeleteObject(sheet);
+            DeleteObject(app);
+            DeleteObject(workbook);
+
+            return dataTable;
+        }
+
+        public static void DataTableToJson(string savePath, DataTable dataTable)
+        {
+            string data = string.Empty;
+            JObject jObjects = new JObject();
+
+            for(int obj = 1; obj < dataTable.Rows.Count; obj++)
+            {
+                JObject sonSpec = new JObject();
+
+                for(int i = 0; i < dataTable.Columns.Count; i++)
+                {
+                    sonSpec.Add(new JProperty(dataTable.Rows[0].ItemArray[i] as string, dataTable.Rows[obj].ItemArray[i]));
+                }
+
+                jObjects.Add(dataTable.Rows[obj].ItemArray[0] as string, sonSpec);
+            }
+
+            data += JsonConvert.SerializeObject(jObjects, Formatting.Indented);
+
+            File.WriteAllText(savePath, data);
+        }
+
+        private static void DeleteObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch(Exception ex)
+            {
+                obj = null;
+                Console.WriteLine(ex.ToString(), obj);
+            }
+        }
+    }
+}
---
+++ ExcelDataConversion/ExcelDataConversion/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelDataConversion
+{
+    public partial class Form1 : Form
+    {
+        DataTable nowDataTable = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            
+            fileDialog.Title = "엑셀파일 오픈 창";
+            fileDialog.FileName = "C:/Users";
+            fileDialog.Filter = "엑셀 파일 (*.*) | *.xlsx*";
+            
+            DialogResult dr = fileDialog.ShowDialog();
+            
+            if(dr == DialogResult.OK)
+            {
+                string fileName = fileDialog.FileName;
+
+                nowDataTable = ExcelDataParser.ExcelFileRead(fileName);
+            }
+        }
+
+        private void Save_Json_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+
+            saveDialog.Title = "Json 저장 창";
+            saveDialog.Filter = "Json 파일 (*.*) | *.json*";
+
+            DialogResult dr = saveDialog.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                string fileName = saveDialog.FileName;
+
+                ExcelDataParser.DataTableToJson(fileName + ".json", nowDataTable);
+            }
+        }
+    }
+}
